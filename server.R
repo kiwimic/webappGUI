28d1184f4ef49f80545145010f6342bc59a0a000
kiwimic <michalsiwik@gmail.com>
2018-07-03 09:53:15 +0200

    Dodanie spisu treści

--- server.R
+++ server.R
@@ -1,12 +1,5 @@
-#
-# This is the server logic of a Shiny web application. You can run the 
-# application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-# 
-#    http://shiny.rstudio.com/
-#
 
+## 0.0.1 wymagane pakiety####
 library(shiny)
 library(ggplot2)
 library(scales)
@@ -16,72 +9,18 @@ library(RSQLite)
 library(lubridate)
 library(data.table)
 
-# myDB_YM <- dbConnect(RSQLite::SQLite(), "C:/Users/msiwik/Desktop/FOLDER R/Analiza_Prepeparatow/Dane/Consensus_YM.sqlite")
-# myDB <- dbConnect(RSQLite::SQLite(), "C:/Users/msiwik/Desktop/FOLDER R/Analiza_Prepeparatow/Dane/Consensus.sqlite") 
-# YM_ALL_WSK <- dbSendQuery(myDB_YM, "SELECT * FROM WSK_YM") %>% fetch()
-# BAZA_CKK <- dbSendQuery(myDB, "SELECT * FROM tab3") %>% fetch()
-
-
-# YM_ALL_WSK_PSEUDO <- YM_ALL_WSK %>%
-  # filter(!is.na(WCSN_PSEUDO)) %>%
-  # filter(WCSN_PSEUDO > 0) %>%
-  # mutate(YMD = ymd(paste0(YM, "-01")))
-
-# YM_PSEUDO_PLOT2 <- YM_ALL_WSK_PSEUDO %>%
-  # mutate(YMD = ymd(paste0(YM, "-01"))) %>%
-  #filter(YMD >=d1, YMD <= d2) %>%
-  # group_by(CKK, YMD) %>%
-  # summarise(WCSN_PSEUDO = sum(WCSN_PSEUDO, na.rm = T)) %>%
-  # group_by(YMD) %>%
-  # mutate(PROC = round(WCSN_PSEUDO/sum(WCSN_PSEUDO),4)) %>%
-  # mutate(WSK = ifelse(PROC >= 0.04, CKK, "Poniżej 4%")) %>%
-  # group_by(YMD, WSK) %>%
-  # summarise(WCSN_PSEUDO = sum(WCSN_PSEUDO, na.rm = T)) %>%
-  # ungroup() %>%
-  # group_by(YMD) %>%
-  # mutate(PROC = round(WCSN_PSEUDO/sum(WCSN_PSEUDO),4)) %>%
-  # arrange(YMD, desc(PROC)) %>%
-  # mutate(CKK = as.numeric(WSK)) %>%
-  # left_join(select(BAZA_CKK, -GPS, -NIP), by = c("CKK"="ID"))
-
-# CKKdoWyboru <<- YM_ALL_WSK_PSEUDO %>%
-  # group_by(CKK) %>%
-  # summarise(WCSN_PSEUDO = sum(WCSN_PSEUDO, na.rm = T)) %>%
-  # arrange(desc(WCSN_PSEUDO)) %>%
-  # filter(WCSN_PSEUDO > 20000) %>%
-  # slice(1:50)
-
-# vline <- function(x = 0, color = "red") {
-  # list(
-    # type = "line", 
-    # y0 = 0, 
-    # y1 = 1, 
-    # yref = "paper",
-    # x0 = x, 
-    # x1 = x, 
-    # line = list(color = color)
-  # )
-# }
-
-# hline <- function(y = 0, color = "blue") {
-  # list(
-    # type = "line", 
-    # x0 = 0, 
-    # x1 = 1, 
-    # xref = "paper",
-    # y0 = y, 
-    # y1 = y, 
-    # line = list(color = color)
-  # )
-# }
-# Define server logic required to draw a histogram
+## 0.1.0 Serwer ####
 shinyServer(function(input, output, session) {
    
+  
+  ## 0.1.1 Scatter plot Wartość w zł vs Udział % wskaźnika psedoefedryny ####
   output$pseudoPlot <- renderPlotly({
     
 
   d1 <- input$dateRange[1]
   d2 <- input$dateRange[2]  
+  p3 <- input$Proc
+  p4 <- input$Wart
     
     dataToPlot <- YM_ALL_WSK_PSEUDO %>%
       mutate(YMD = ymd(paste0(YM, "-01"))) %>%
@@ -117,6 +56,7 @@ shinyServer(function(input, output, session) {
       config(displayModeBar = F)
     
   })
+  ## 0.1.2 ####
   output$pseudoPlotYM <- renderPlotly({
     
     
@@ -137,6 +77,7 @@ shinyServer(function(input, output, session) {
       
   })
   
+  ## 0.1.3 ####
   output$pseudoPlotYM_CKK <- renderPlotly({
     
     if (length(input$in6) > 10) {
@@ -165,16 +106,19 @@ shinyServer(function(input, output, session) {
   
   })
   
+  ## 0.1.4 ####
   output$hover <- renderPrint({
     d <- event_data("plotly_hover")
     if (is.null(d)) "Hover events appear here (unhover to clear)" else d
   })
   
+  ## 0.1.5 ####
   output$click <- renderPrint({
     d <- event_data("plotly_click")
     if (is.null(d)) "Click events appear here (double-click to clear)" else d
   })
   
+  ## 0.1.6 ####
   output$brush <- renderDataTable({
     d <- event_data("plotly_selected", source = "scatterZaznacz")
     if (is.null(d)) {
@@ -201,6 +145,7 @@ shinyServer(function(input, output, session) {
         }
   })
   
+  ## 0.1.7 ####
   output$select2 <- renderPlotly({
     d <- event_data("plotly_selected", source = "scatterZaznacz")
     if (is.null(d)) {
