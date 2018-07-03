928f218916f3dcd8a53924d78bbbf53ca5e0e872
kiwimic <michalsiwik@gmail.com>
2018-07-03 22:24:08 +0200

    Dodana funkcja do przetwarzania danych do scatterplotu
    YM_ALL_WSK ma teraz mutate YMD

--- global.r
+++ global.r
@@ -5,12 +5,12 @@ library(dplyr)
 library(plotly)
 library(RSQLite)
 library(lubridate)
+library(rlang) ## rlang::sym
 library(data.table)
-
 
 myDB_YM <- dbConnect(RSQLite::SQLite(), "C:/Users/msiwik/Desktop/FOLDER R/Analiza_Prepeparatow/Dane/Consensus_YM.sqlite")
 myDB <- dbConnect(RSQLite::SQLite(), "C:/Users/msiwik/Desktop/FOLDER R/Analiza_Prepeparatow/Dane/Consensus.sqlite") 
-YM_ALL_WSK <- dbSendQuery(myDB_YM, "SELECT * FROM WSK_YM") %>% fetch()
+YM_ALL_WSK <- dbSendQuery(myDB_YM, "SELECT * FROM WSK_YM") %>% fetch() %>% mutate(YMD = ymd(paste0(YM, "-01")))
 BAZA_CKK <- dbSendQuery(myDB, "SELECT * FROM tab3") %>% fetch()
 
 ### Takie zapytanie to około 30 sekund na moim komputerze
@@ -32,6 +32,15 @@ BAZA_CKK <- dbSendQuery(myDB, "SELECT * FROM tab3") %>% fetch()
 #   summarise(WCSN = sum(WCSN, na.rm = T)) %>%
 #   collect()
 # Sys.time()  
+
+
+tbl(myDB, "tab1") %>%
+     filter(CKK == 123812) %>%
+   group_by(CKK, CKT, DATA_ZAFAKTUROWANIA) %>%
+   summarise(WCSN = sum(WCSN, na.rm = T)) %>%
+   collect() %>%
+   mutate(YMD_HMS = as.POSIXct(DATA_ZAFAKTUROWANIA, origin = "1970-01-01 00:00:00 UTC"))-> test_123812
+
 
 YM_ALL_WSK_PSEUDO <- YM_ALL_WSK %>%
   filter(!is.na(WCSN_PSEUDO)) %>%
@@ -61,6 +70,7 @@ CKKdoWyboru <<- YM_ALL_WSK_PSEUDO %>%
   filter(WCSN_PSEUDO > 20000) %>%
   slice(1:50)
 
+## Rysowanie poziomych lini na wykresie plotly ####
 vline <- function(x = 0, color = "red") {
   list(
     type = "line", 
@@ -73,6 +83,7 @@ vline <- function(x = 0, color = "red") {
   )
 }
 
+## Rysowanie pionowych lini na wykresie plotly ####
 hline <- function(y = 0, color = "blue") {
   list(
     type = "line", 
@@ -83,4 +94,58 @@ hline <- function(y = 0, color = "blue") {
     y1 = y, 
     line = list(color = color)
   )
+}
+
+filter_col <- function(df, col_name_as_string, val){
+  col_name <- rlang::sym(col_name_as_string)
+  df %>% filter((!!col_name) == val)
+}
+
+DaneDoScatter(dane = YM_ALL_WSK,
+              input_data_start = "2018-01-01",
+              input_data_koniec = "2018-11-01",
+              Wart_COL = "WCSN_DEF",
+              WSK_COL = "WSK_DEF"
+              ) -> test
+## Scatter plot plotly #####
+DaneDoScatter <- function(dane,
+                          WSK, 
+                          input_data_start,
+                          input_data_koniec,
+                          input_proc = 40,
+                          input_wart = 50,
+                          Wart_COL = "WCSN_PSEUDO",
+                          WSK_COL = "WSK_PSEUDO") {
+  
+  
+  sym_Wart_COL <- rlang::sym(Wart_COL)
+  sym_WSK_COL <- rlang::sym(WSK_COL)
+  
+  
+  dataToPlot <- dane %>%
+    filter(!is.na(!!sym_Wart_COL)) %>%
+    mutate(YMD = ymd(paste0(YM, "-01"))) %>%
+    filter(YMD >= ymd(input_data_start),
+           YMD <= ymd(input_data_koniec)) %>%
+    group_by(CKK) %>%
+    summarise(WCSN_ALL = sum(WCSN_ALL, na.rm = T),
+              (!!sym_Wart_COL) := sum((!!sym_Wart_COL), na.rm = T)) %>%
+   mutate((!!sym_WSK_COL) := (!!sym_Wart_COL)/WCSN_ALL) %>%
+   mutate((!!sym_WSK_COL) := ifelse((!!sym_WSK_COL) > 1, 1, (!!sym_WSK_COL)),
+          (!!sym_WSK_COL) := ifelse((!!sym_WSK_COL) < 0, 0, (!!sym_WSK_COL))) %>%
+  filter((!!sym_WSK_COL) >= input_proc/100) %>%
+  filter((!!sym_Wart_COL) >= input_wart  * 1000) %>%
+  left_join(select(BAZA_CKK, -GPS, -NIP), by = c("CKK"="ID")) %>%
+  mutate(LP = (1:n())-1) -> ret
+  
+  return(ret)
+  # d1 <- input$dateRange[1]
+  # d2 <- input$dateRange[2]  
+  # p3 <- input$Proc
+  # p4 <- input$Wart
+}
+
+
+ScatterPlotly <- function() {
+  
 }
